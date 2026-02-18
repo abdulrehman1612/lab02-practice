498011ac6a5dd75bd0960bc4dbf5eadd029e209a
AGU <rehmaaan.ch@gmail.com>
2026-02-18 19:58:01 +0500

    Add math tools with unit tests

---
+++ math_tools.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 18 14:15:46 2026
+
+@author: AGU
+"""
+
+def add(a, b):
+    return a + b
+
+def multiply(a, b):
+    return a * b
+
+def is_even(n):
+    return n % 2 == 0
